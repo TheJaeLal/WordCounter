b63a7e0979e79ae8c85302d8801072ac3ea5573b
thejaelal <jay.lal@somaiya.edu>
2018-01-11 11:55:11 +0530

    Complete and working

--- Word-Counter.py
+++ Word-Counter.py
@@ -1,5 +1,20 @@
 import sys
+import re
 
-file_name = sys.argv[1]
+file_name = ""
 
-with open()
+if len(sys.argv)>1:
+	file_name = sys.argv[1]
+else:
+	print("Please pass the file name/path as a command line argument..")
+	print("eg. python Word-Counter.py /path/to/text-file")
+	exit(0)
+
+with open(file_name,'r') as f:
+	text = f.read()
+
+words = re.split('\W+', text)
+
+print(words)
+
+print("Number of words in text =",len(words))
